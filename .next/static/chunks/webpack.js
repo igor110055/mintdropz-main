4416a9d29eb53d40355cf7840205c239f02d73ca
hanif365 <m.a.hanifkhan64@gmail.com>
2022-05-17 07:40:54 +0600

    video border radius uppdate for mobile device

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a6ddcde910425256"; }
+/******/ 		__webpack_require__.h = function() { return "65ebd75d050df11d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
